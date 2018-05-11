a95baa2e10be06acfd062e8f54a03a322cf7ba3c
Alex Pickering <33042726+MechaFinch@users.noreply.github.com>
2018-05-11 13:56:15 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-document.getElementById('button')[0].onclick = function() {
+function showHelp() {
   alert("Help button clicked");
   alert("To use the tools given, drag the links into a bookmark\nThis creates a bookmarklet, or a script you can run anywhere by clicking the bookmark\nI recommend placing the bookmarks in a folder");
-};
+}
