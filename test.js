5b8c7ab9948f85c1cf6a6e877c2401cc62a417a1
Alex Pickering <33042726+MechaFinch@users.noreply.github.com>
2018-05-13 20:26:01 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
 function showHelp() {
-  alert("Help button clicked");
   alert("To use the tools given, drag the links into a bookmark\nThis creates a bookmarklet, or a script you can run anywhere by clicking the bookmark\nI recommend placing the bookmarks in a folder");
 }
